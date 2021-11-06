0839dbe1f826ecaf4c72d50b1c6925d6818edc0d
Louis <bellefemine.louis@gmail.com>
2021-11-06 16:32:20 +0100

    merge fix + cleanup

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,14 @@
-<template>
-  <div id="nav">
+<!-- Vue router to implement
+<div id="nav">
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
   </div>
   <router-view/>
-  <FooterAccueil/>
+-->
+
+<template>
+  <div id="main">
+    <FooterAccueil/>
+  </div>
 </template>
 
 <script>
