c41a4a0e1158b28782c4b007eb26bdb52a215724
YannisGJ <93491318+YannisGJ@users.noreply.github.com>
2021-11-20 16:33:04 +0100

    Init Branch mid Acceuil

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,59 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <MidAcceuil msg="Resources relationnelles "/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MidAcceuil from './components/MidAcceuil.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    MidAcceuil
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+h1 {  
+      margin-left: 100px;
+      margin-top: 200px;
+      margin-bottom: 50px;
+      font-size: 40px;
+      color: #296870;
+      font-family: system-ui;
 }
+p{
+  margin-left: 100px;
+  font-size: 20px;
+  color: #296870;
+}
+.ImageHome{
+      width: 800px;
+      margin-left: 800px;
+      margin-top: -460px;
+}
+.Button1{
+      background-image: url(assets/LogoGoolgePlay.png);
+      background-size: 135px,135px;
+      background-repeat: no-repeat;
+      background-position: 10px,10px;
+      width: 150px;
+      height: 55px;
+}
+.Button{
+      background-color: #296870
+;
+      border: none;
+      border-radius: 10px;
+      color: white;
+      padding: 20px 34px;
+      text-align: center;
+      text-decoration: none;
+      display:inline-block;
+      font-size: 15px;
+      cursor: pointer;
+      margin-left: 110px;
+      margin-top: 40px;
+      font-weight: bold;
+         }       
 </style>
---
+++ src/components/MidAcceuil.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="MidAcceuil">
+    <h1>{{ msg }}</h1>
+    <p>
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit,<br>
+       sed do eiusmod tempor incididunt ut labore et dolore magna<br>
+       aliqua. Ut enim ad minim veniam, quis nostrud exercitation <br>
+       ullamco laboris nisi ut aliquip ex ea commodo consequat. <br>
+       Duis aute irure dol esse cillum dolore eu fugiat nulla <br>
+       pariatur. Excepteur sint occaecat cupidatat non proident,<br>
+       sunt in culpa qui officia deserunt mollit anim"<br>
+    </p>
+    <button class="Button Button1" type="button"></button>
+    <button class="Button Button2" type="button">Nous Rejoindre</button>
+    </div>
+  <img class="ImageHome" src="../assets/HomePageGroup.png" alt="Image Home page">
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+
+<style scoped>
+</style>
