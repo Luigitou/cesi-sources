772b9b01a9c6637ec672567953c4511016abf067
GARZUEL-JOLY YANNIS <yannis.garzueljoly@viacesi.fr>
2021-12-02 13:14:26 +0100

    Base mid Brache Tdb + style to do V1

--- src/components/TDB/Sliders.vue
+++ src/components/TDB/Sliders.vue
@@ -1,13 +1,17 @@
 <template>
     <div class="Sliders">
        <p>{{Text}}</p>
-       <div class="wrapperImages" v-for="images in Img" :key="images">
-         <Images v-bind:Text='images["Text"]' />
+       <div class="wrapperbasementImage">
+         <div class="wrapperImages" v-for="images in Img" :key="images">
+           <Images v-bind:Text='images["Text"]' />
+         </div>
        </div>
-       
-        <span class="dot" onclick="currentSlide(1)"></span>
-      <span class="dot" onclick="currentSlide(2)"></span>
-      <span class="dot" onclick="currentSlide(3)"></span>
+       <div class="wrapperDot">
+
+         <span class="dot" onclick="currentSlide(1)"></span>
+         <span class="dot" onclick="currentSlide(2)"></span>
+         <span class="dot" onclick="currentSlide(3)"></span>
+       </div>  
     </div>
 </template>
 
@@ -32,15 +36,43 @@ export default {
 .Sliders{
   display: flex;
   justify-content: center;
+  flex-direction: column;
+  align-items: center;
+
+p{
+  text-decoration: underline;
+}
+
+.wrapperbasementImage{
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
 
   .wrapperImages{
       display:flex;
       flex-direction: row;
+    }
   }
+  .wrapperDot{
+  display: flex;
+  flex-direction: row;
 
-  span {
-      display: block;
-  }
+    .dot {
+  cursor: pointer;
+  height: 15px;
+  width: 15px;
+  border-radius: 50%;
+  background-color: #037682;
+  transition: background-color 0.6s ease;
+
+    &:hover {
+  background-color: #FF914D;
+      }
+    }
+  } 
 }
+
+
+
 
 </style>
