1e49c2be5bcec4f7ab6237e655ead448d52918a3
Louis <bellefemine.louis@gmail.com>
2022-05-15 19:45:45 +0200

    sidemnu layout

---
+++ src/components/navigation/sidemenu/Menu.vue
@@ -0,0 +1,59 @@
+<template>
+  <nav class="menu">
+    <div class="item" v-for="link in links" :key="link">
+      <a class="textMenu" :href="'#/' + link.to">
+        <i :class="link.icon"></i>{{ link.label }}
+      </a>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "Menu",
+  data() {
+    return {
+      links: [
+        {
+          label: "Vos fichiers",
+          icon: "pi pi-file iconR",
+          to: "vosfichiers",
+        },
+        {
+          label: "Vos amis",
+          icon: "pi pi-users iconR",
+          to: "vosamis",
+        },
+        {
+          label: "Vos Favoris",
+          icon: "pi pi-heart iconR",
+          to: "vosfav",
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.menu {
+  .item {
+    padding: 2rem 0.5rem;
+
+    .textMenu {
+      font-size: 110%;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      gap: 0.5rem;
+      justify-content: center;
+      cursor: pointer;
+
+      .iconR {
+        margin-right: 0.5rem;
+        font-size: 200%;
+      }
+    }
+  }
+}
+</style>
--- src/components/navigation/sidemenu/Sidemenu.vue
+++ src/components/navigation/sidemenu/Sidemenu.vue
@@ -1,17 +1,32 @@
 <template>
-  <div class="sidemenu">Sidemenu</div>
+  <div class="sidemenu">
+    <img class="logo" src="../../../assets/Logo_projet.png" alt="Logo" />
+    <Menu />
+    <!--<FooterMenu />-->
+  </div>
 </template>
 
 <script>
+import Menu from "./Menu.vue";
+//import FooterMenu from "./FooterMenu.vue";
+
 export default {
   name: "Sidemenu",
-  components: {},
+  components: {
+    Menu,
+    //FooterMenu,
+  },
 };
 </script>
 
 <style lang="scss" scoped>
 .sidemenu {
-  background-color: coral;
+  background-color: #effaff;
   height: 100%;
+  position: relative;
+
+  .logo {
+    width: 100%;
+  }
 }
 </style>
